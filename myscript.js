2901b346efa9e3ce38bdc2f1df8ca26ffef9c6ba
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-02 14:23:52 +0100

    creata password

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+let userInputName = prompt("Qual'è il tuo nome ?");
+let userInputOld = prompt("Quanti anni hai ?");
+let userInputColor = prompt("Qual'è il tuo colore preferito?");
+
+let userData = userInputName + userInputOld + userInputColor;
+const number = 21;
+
+let userPassoword = userData + number;
+
+document.getElementById("user_pass").innerHTML = userPassoword;
+
